3e1c7e8e14b505a6ccddc2dc49427c0c1c5a64f9
armorik83 <okunokentaro@me.com>
2015-02-07 22:20:23 +0900

    init example/

---
+++ example/src/app.ts
@@ -0,0 +1,14 @@
+/**
+ * cw-modal Sample App
+ *
+ * @copyright © 2015 Crescware
+ * @since cw-modal v 0.0.1 (Feb 7, 2015)
+ */
+/// <reference path="../../typings/angularjs/angular.d.ts" />
+/// <reference path="../../typings/node/node.d.ts" />
+'use strict';
+
+// Loading angular for global variable.
+// Cannot write as var angular = require('angular');
+// because of overwrite a global variable.
+require('angular');
