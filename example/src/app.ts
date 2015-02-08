25b347feb74cae8f6e090d11355e7e2680c06385
armorik83 <okunokentaro@me.com>
2015-02-08 14:41:15 +0900

    feat(example): create main

--- example/src/app.ts
+++ example/src/app.ts
@@ -5,6 +5,7 @@
  * @since cw-modal v 0.0.1 (Feb 7, 2015)
  */
 /// <reference path="../../typings/angularjs/angular.d.ts" />
+/// <reference path="../../typings/angularjs/angular-route.d.ts" />
 /// <reference path="../../typings/node/node.d.ts" />
 'use strict';
 
@@ -12,3 +13,22 @@
 // Cannot write as var angular = require('angular');
 // because of overwrite a global variable.
 require('angular');
+require('angular-route');
+
+export var appName = 'cwModalExample';
+export var externalModule = ['ngRoute'];
+angular.module(appName, externalModule);
+
+function RouteConfig(
+  $routeProvider: ng.route.IRouteProvider,
+  $locationProvider: ng.ILocationProvider
+) {
+  $locationProvider.html5Mode(true);
+  $routeProvider
+    .when('/', {templateUrl: 'src/views/main.html'})
+    .otherwise({redirectTo: '/'});
+}
+RouteConfig.$inject = ['$routeProvider', '$locationProvider'];
+
+angular.module(appName).config(RouteConfig);
+
