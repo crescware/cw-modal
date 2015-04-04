eae5247803d28b45b205a04194b7ea1fe44dd389
armorik83 <okunokentaro@me.com>
2015-04-04 15:33:39 +0900

    chore(gulpfile): update

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,7 @@
 'use strict';
 var del = require('del');
 var espower = require('gulp-espower');
+var glob = require('glob');
 var gulp = require('gulp');
 var seq = require('run-sequence');
 var shell = require('gulp-shell');
@@ -25,12 +26,12 @@ gulp.task('clean', del.bind(null, [
 
 /* ts */
 var tsc = 'tsc -m commonjs -t es6 --noImplicitAny';
-gulp.task('ts:lib_',     shell.task([`find ${opt.lib}/**     -name "*.ts" | xargs ${tsc}`]));
-gulp.task('ts:example_', shell.task([`find ${opt.example}/** -name "*.ts" | xargs ${tsc}`]));
+gulp.task('ts:example_', shell.task([`find ${opt.example} -name *.ts | xargs ${tsc}`]));
+gulp.task('ts:lib_',     shell.task([`find ${opt.lib}     -name *.ts | xargs ${tsc}`]));
 
-gulp.task('ts:lib',     function(cb) {seq('clean', 'ts:lib_', cb)});
-gulp.task('ts:example', function(cb) {seq('clean', 'ts:example_', cb)});
-gulp.task('ts',         function(cb) {seq('clean', ['ts:lib_', 'ts:example_'], cb)});
+gulp.task('ts:example', function(done) {seq('clean', 'ts:example_', done)});
+gulp.task('ts:lib',     function(done) {seq('clean', 'ts:lib_', done)});
+gulp.task('ts',         function(done) {seq('clean', ['ts:lib_', 'ts:example_'], done)});
 
 /* babel */
 gulp.task('babel:example', shell.task([`babel ${opt.example} --out-dir ${opt.example}`]));
@@ -39,16 +40,35 @@ gulp.task('babel:test',    shell.task([`babel ${opt.test}    --out-dir ${opt.tes
 gulp.task('babel', ['babel:example', 'babel:lib']);
 
 /* browserify */
-var ify = `-r licensify -o ${opt.example}/src/bundle.js`;
-gulp.task('watch_', shell.task([`find ${opt.example}/** -name "*.js" | xargs watchify ${ify} -v`]));
-gulp.task('watch', function(cb) {seq('ts', 'babel', 'watch_', cb)});
+function globToBrowserify(bundler) {
+  var verbose = (bundler === 'watchify')? '-v' : '';
+  return function(done) {
+    var p = new Promise(function(resolve) {
+      glob(`${opt.example}/**/*.js`, function(er, files) {
+        resolve(files.join(' '));
+      });
+    });
+    p.then(function(names) {
+      shell.task([`${bundler} ${names} -o ${opt.example}/src/bundle.js ${verbose}`])();
+      done();
+    });
+  };
+}
 
-gulp.task('browserify_', shell.task([`find ${opt.example}/** -name "*.js" | xargs browserify ${ify}`]));
-gulp.task('browserify', function(cb) {seq('ts', 'babel', 'browserify_', cb)});
+gulp.task('watchify_',   globToBrowserify('watchify'));
+gulp.task('browserify_', globToBrowserify('browserify'));
+
+gulp.task('watchify',   function(done) {seq('ts', 'babel', 'watchify_', done)});
+gulp.task('browserify', function(done) {seq('ts', 'babel', 'browserify_', done)});
+
+/* watch */
+gulp.task('watch', ['watchify', 'ts'], function() {
+  gulp.watch([`${opt.example}/**/*.ts`, `${opt.lib}/**/*.ts`], ['ts']);
+});
 
 /* build */
-gulp.task('build_', shell.task([`find ${opt.lib}/** -name "*.js" | xargs browserify --exclude angular --standalone CwModal > bundle.js`]));
-gulp.task('build', function(cb) {seq('ts:lib', 'babel:lib', 'build_', cb)});
+gulp.task('build_', shell.task([`find ${opt.lib} -name *.js | xargs browserify --exclude angular --standalone CwModal > bundle.js`]));
+gulp.task('build', function(done) {seq('ts:lib', 'babel:lib', 'build_', done)});
 
 /* test */
 gulp.task('espower', function() {
@@ -57,4 +77,4 @@ gulp.task('espower', function() {
     .pipe(gulp.dest(opt.testEspowered));
 });
 gulp.task('test_', shell.task([`mocha ${opt.testEspowered}`]));
-gulp.task('test', function(cb) {seq('ts:lib_', ['babel:lib', 'babel:test'], 'espower', cb)});
+gulp.task('test', function(done) {seq('ts:lib_', ['babel:lib', 'babel:test'], 'espower', done)});
